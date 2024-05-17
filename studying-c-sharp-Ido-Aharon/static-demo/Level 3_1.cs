99b03f7d522dafd99a9a3c6349da281a6791c64f
Ido Aharon <ido991155@gmail.com>
2024-05-17 11:59:30 +0300

    my work

---
+++ studying-c-sharp-Ido-Aharon/static-demo/Level 3_1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studying_c_sharp_Ido_Aharon
+{
+    public class Level_3_1
+    {
+        public static void RunMe()
+        {
+            Console.WriteLine("“Hi, I am class Level_3_1");
+        }
+    }
+}
---
+++ studying-c-sharp-Ido-Aharon/static-demo/Level2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studying_c_sharp_Ido_Aharon
+{
+   public class Level2
+    {
+        public static void RunMe()
+        {
+            Console.WriteLine("“Hi, I am class Level2");
+        }
+    }
+}
